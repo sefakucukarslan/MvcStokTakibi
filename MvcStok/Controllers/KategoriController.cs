22db9f2ad745430fddd8aedf399733852f25b762
sefak <sefak@DESKTOP-J15C4G9>
2022-11-17 10:51:58 +0300

    Güncelleme işlemleri yapıldı

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -27,6 +27,10 @@ namespace MvcStok.Controllers
         //View tarafında form etiketi içerisine method post olarak açılmalı açılan o post burayı tetikleyecek.
         public ActionResult YeniKategori(TBLKATEGORILER p1)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
             db.TBLKATEGORILER.Add(p1);
             db.SaveChanges();
             return View();
@@ -35,6 +39,20 @@ namespace MvcStok.Controllers
         {
             var kategori = db.TBLKATEGORILER.Find(id);
             db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir",ktgr);
+        }
+
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- MvcStok/Controllers/MusteriController.cs
+++ MvcStok/Controllers/MusteriController.cs
@@ -28,11 +28,23 @@ namespace MvcStok.Controllers
             db.SaveChanges();
             return View();
         }
-
         public ActionResult SIL(int id)
         {
             var musteri = db.TBLMUSTERILER.Find(id);
             db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
